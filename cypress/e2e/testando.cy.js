40260503fd7c37d7433d452b0860fcda930382aa
vic-3PO <victma2001@gmail.com>
2023-11-20 13:03:56 -0300

    Criando teste de registro no arquivo testando.cy.js

--- cypress/e2e/testando.cy.js
+++ cypress/e2e/testando.cy.js
@@ -2,5 +2,10 @@ describe("template spec", () => {
   it("passes", () => {
     cy.visit("http://localhost:4200/#/home");
     cy.contains("a", "Register now").click();
+    cy.get('[formcontrolname="email"]').type("victor@email.com.br");
+    cy.get('[formcontrolname="fullName"]').type("Victor Lopes");
+    cy.get('[formcontrolname="userName"]').type("victorl");
+    cy.get('[formcontrolname="password"]').type("victor123");
+    cy.contains("button", "Register").click();
   });
 });
