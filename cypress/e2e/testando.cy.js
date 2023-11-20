1b50f565c0dc003f312547215de1c35584fc638c
vic-3PO <victma2001@gmail.com>
2023-11-20 13:09:55 -0300

    Refatorar teste para a página de cadastro, melhorar seletores HTML usando data-test, e atualizar descrições conforme boas práticas do Cypress

--- cypress/e2e/testando.cy.js
+++ cypress/e2e/testando.cy.js
@@ -1,11 +1,11 @@
-describe("template spec", () => {
-  it("passes", () => {
+describe("Página de cadastro", () => {
+  it("Preencher os campos do formulário corretamente para cadastrar um novo usuário", () => {
     cy.visit("http://localhost:4200/#/home");
     cy.contains("a", "Register now").click();
-    cy.get('[formcontrolname="email"]').type("victor@email.com.br");
-    cy.get('[formcontrolname="fullName"]').type("Victor Lopes");
-    cy.get('[formcontrolname="userName"]').type("victorl");
-    cy.get('[formcontrolname="password"]').type("victor123");
+    cy.get('[data-test="email"]').type("victor@email.com.br");
+    cy.get('[data-test="fullName"]').type("Victor Lopes");
+    cy.get('[data-test="registerUserName"]').type("victorl");
+    cy.get('[data-test="registerPassword"]').type("victor123");
     cy.contains("button", "Register").click();
   });
 });
