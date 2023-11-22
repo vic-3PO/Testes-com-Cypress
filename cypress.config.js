00eb9d8c8bd40711e0deed198ea2de00e451f75e
vic-3PO <victma2001@gmail.com>
2023-11-22 11:10:21 -0300

    Configurar e executar testes usando o mochawesome para gerar relatórios

--- cypress.config.js
+++ cypress.config.js
@@ -3,7 +3,18 @@ const { defineConfig } = require("cypress");
 module.exports = defineConfig({
   e2e: {
     setupNodeEvents(on, config) {
-      // implement node event listeners here
+      {
+        ("reporter");
+        "mochawesome", "reporterOptions";
+        {
+          ("reportDir");
+          "cypress/report/mochawesome-report", "overwrite";
+          true, "html";
+          true, "json";
+          false, "timestamp";
+          ("mmddyyyy_HHMMss");
+        }
+      }
     },
   },
 });
