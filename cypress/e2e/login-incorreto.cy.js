6c78398c378d15d9a15bea030c9a1c7aaeb5fd8b
vic-3PO <victma2001@gmail.com>
2023-11-23 11:39:14 -0300

    Adiciona teste de login incorreto com stub para simulação de erro na interação com a API

--- cypress/e2e/login-incorreto.cy.js
+++ cypress/e2e/login-incorreto.cy.js
@@ -1,7 +1,17 @@
 describe("Página de login", () => {
+  beforeEach(() => {
+    cy.visit("http://localhost:4200");
+    cy.intercept("POST", "http://localhost:3000/user/login", {
+      statusCode: 400,
+    }).as("stubPost");
+  });
+
   it("Verificar mensagem de campos obrigatórios", () => {
-    cy.visit("http://localhost:4200/#/home");
     cy.contains("User name is required!").should("be.visible");
     cy.contains("Password is required!").should("be.visible");
+  });
+  it("Deve falhar mesmo que os campos sejam preenchidos corretamente", () => {
+    cy.login("victorl", "victor123");
+    cy.wait("@stubPost");
   });
 });
